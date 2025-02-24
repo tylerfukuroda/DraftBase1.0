aaadb4a91087033c621e7a6b5aa42091e1e5fafb
tylerfukuroda <155584079+tylerfukuroda@users.noreply.github.com>
2025-02-23 20:42:09 -0800

    Update WebConfig.java

--- java/com/data/draftbase/WebConfig.java
+++ java/com/data/draftbase/WebConfig.java
@@ -10,11 +10,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/players")
-        		.allowedHeaders("*")
-                .allowedOrigins("http://localhost:5500", "http://127.0.0.1:5500")
-                .allowCredentials(true)
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
         
     }
 }
